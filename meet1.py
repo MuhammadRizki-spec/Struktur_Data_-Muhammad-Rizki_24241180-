749d3f34a41b73f25d7725bb0f1622e42a13b13a
Rizky <isky9352@gmail.com>
2025-04-11 09:53:20 +0800

    Add files via upload

---
+++ meet1.py
@@ -0,0 +1,35 @@
+# perbandingan kemungkinan nilai = true/false (bolean)
+# 1 > 2 =
+# <, >=, <=, !=, not
+# a = 24
+# b = 80
+
+a = 24 
+b = 80
+hasil = a < b
+print(hasil)
+
+a = 24
+b = 80
+hasil = a > b
+print(hasil)
+
+a = 24
+b = 80
+hasil = a >= b
+print(hasil)
+
+a = 24 
+b = 80
+hasil = a <= b
+print(hasil)
+
+a = 24
+b = 80
+hasil = a != b
+print(hasil)
+
+a = 24 
+b = 80
+hasil = a not b
+print(hasil)
